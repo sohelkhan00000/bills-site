1dced5173af7a69bedf10bbfd41e469d912adc48
sohel khan <sohelkhan00000@gmail.com>
2023-07-12 10:23:45 +0530

    new page added

--- assets/js/main-page.js
+++
@@ -1,6 +0,0 @@
-let moblNav = document.getElementById("nav-links");
-let linkbtn = document.getElementById("linkmblmenu");
-
-linkbtn.addEventListener("click", () => {
-    moblNav.classList.toggle("d-block");
-});
