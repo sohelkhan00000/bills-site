536adb1ac56d14734166d784d15929b22461170f
sohel khan <sohelkhan00000@gmail.com>
2023-07-17 15:21:34 +0530

    js changes

--- assets/js/all-page.js
+++ assets/js/all-page.js
@@ -1,6 +1,1 @@
-const pageWrapper = document.getElementById("pageWrapper");
-const widgetWrapper = document.getElementById("widgetWrapper");
-
-
-pageWrapper.setAttribute("class","col-md-12");
-widgetWrapper.classList.add("d-none");
+const pageWrapper=document.getElementById("pageWrapper"),widgetWrapper=document.getElementById("widgetWrapper");pageWrapper.setAttribute("class","col-md-12"),widgetWrapper.classList.add("d-none");
