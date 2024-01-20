3b5994acf69296b13870ec8a1a9b340da207e2af
sohel khan <sohelkhan00000@gmail.com>
2024-01-20 20:47:02 +0530

    internet invoice page edited

--- assets/js/partialPage/fuel-bill-data.js
+++ assets/js/partialPage/fuel-bill-data.js
@@ -130,7 +130,7 @@ var template ={
         <!-- select box -->
 
         <label for="u-vehicle-type" class="form-label">Vehicle Type</label>
-        <select class="form-control" id="u-vehicle-type">
+        <select class="form-select" id="u-vehicle-type">
             <option>Petrol</option>
             <option>Deisel</option>
             <option>CNG</option>
@@ -140,7 +140,7 @@ var template ={
     </div>
     <div class="col-lg-6 col-md-12 mt-3">
         <label for="u-payment-type" class="form-label">Payment Type</label>
-        <select class="form-control" id="u-payment-type">
+        <select class="form-select" id="u-payment-type">
             <option>Cash</option>
             <option>Online</option>
             <option>Debit Card</option>
