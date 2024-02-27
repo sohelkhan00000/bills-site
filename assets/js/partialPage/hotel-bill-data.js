8d0c418879d6af9109d70b473dedc6fed2cba73e
sohel <sohel.khan@bajajcapital.com>
2024-02-27 09:41:26 +0530

    all angular remvoe

---
+++ assets/js/partialPage/hotel-bill-data.js
@@ -0,0 +1,31 @@
+var hotelBillData = {
+    "template-2":`    <div data-tm="template-2" class="templates template-2 rest-tm-container">
+    <div class="hotel-bill">
+        <div class="row">
+            <div class="col-12">
+                <h6>Hello Template 2</h6>
+            </div>
+        </div>
+    </div>
+</div>`,
+"form-control":`<div id="dynamic-forms-controls"></div>`,
+"modal": `     <div class="modal fade template-modal modal-template" id="modal-template" tabindex="-1"
+aria-labelledby="tester-view">
+<div class="modal-dialog modal-fullscreen">
+    <div class="modal-content">
+        <!-- <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"
+                aria-label="Close"></button>
+        </div> -->
+        <div class="modal-body">
+
+        </div>
+        <div class="modal-footer d-block d-md-none text-end bg-body">
+            <button type="button" class="btn btn-success"
+                data-bs-dismiss="modal">Close</button>
+        </div>
+    </div>
+</div>
+</div>`
+}
